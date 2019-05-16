3d43ecf3c5820db010259e04c1b4bb02646d180d
Daniil Novikau <novikaud@gmail.com>
2019-05-16 17:34:47 +0300

    Add 'checkTicketId' button to asmt modal

--- src/ggrc-client/js/components/issue-tracker/modal-issue-tracker-fields.js
+++ src/ggrc-client/js/components/issue-tracker/modal-issue-tracker-fields.js
@@ -6,12 +6,30 @@
 import '../dropdown/dropdown-component';
 import '../numberbox/numberbox-component';
 import template from './templates/modal-issue-tracker-fields.stache';
+import {
+  checkIssueTrackerTicketId,
+} from '../../plugins/utils/issue-tracker-utils';
+import {
+  validateAttr,
+} from '../../plugins/utils/validation-utils';
 
 export default can.Component.extend({
   tag: 'modal-issue-tracker-fields',
   view: can.stache(template),
   leakScope: true,
   viewModel: can.Map.extend({
+    define: {
+      hasError: {
+        get() {
+          return this.attr('isChecked') && !this.attr('isValid');
+        },
+      },
+      validationError: {
+        get() {
+          return validateAttr(this.instance, 'issue_tracker.issue_id');
+        },
+      },
+    },
     instance: {},
     note: '',
     linkingNote: '',
@@ -19,6 +37,10 @@ export default can.Component.extend({
     setIssueTitle: false,
     allowToChangeId: false,
     isTicketIdMandatory: false,
+    isCheckInProgress: false,
+    isChecked: false,
+    isValid: false,
+    msg: '',
     setTicketIdMandatory() {
       let instance = this.attr('instance');
 
@@ -27,6 +49,26 @@ export default can.Component.extend({
           instance.class.unchangeableIssueTrackerIdStatuses
             .includes(instance.attr('status')));
       }
+    },
+    resetValidationStatus() {
+      this.attr('isChecked', false);
+      this.attr('isValid', false);
+    },
+    checkTicketId() {
+      this.resetValidationStatus();
+      this.attr('isCheckInProgress', true);
+
+      const instance = this.attr('instance');
+      if (instance.issue_tracker.issue_id) {
+        return checkIssueTrackerTicketId(instance.issue_tracker.issue_id)
+          .then((data) => {
+            this.attr('isValid', data.valid);
+            this.attr('msg', data.msg);
+            this.attr('isCheckInProgress', false);
+            this.attr('isChecked', true);
+          });
+      }
+      return false;
     },
   }),
   events: {
